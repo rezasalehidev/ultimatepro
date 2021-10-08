ec3883a36e30b05e20b7e7abe487ea9941aa1cc1
rezasalehi <rezasalehidev@gmail.com>
2021-10-08 20:36:54 +0330

    first step for project

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import { css } from '@emotion/css'
+
+const color = 'white'
 
 function HomePage() {
-  return <div className="main">Index Page</div>
+  return (
+    <>
+      <div className="main">Index Page</div>
+      <div
+        className={css`
+          padding: 32px;
+          background-color: hotpink;
+          font-size: 24px;
+          border-radius: 4px;
+          &:hover {
+            color: ${color};
+          }
+        `}
+      >
+        Hover to change color.
+      </div>
+    </>
+  )
 }
 
 export default HomePage
